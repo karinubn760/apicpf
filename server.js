21558f643d3a1bd834f30e1fd09b3cd2e3345234
karinubn760 <karinubn760@proton.me>
2025-02-21 13:39:23 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+cors({ origin: '*' });
 
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
